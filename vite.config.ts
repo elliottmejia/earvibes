15629c01104601ebd11977869b8c5a59c57e1266
Elliott Mejia <mejia.elliott@gmail.com>
2025-12-01 18:48:06 -0800

    - Removed CDN link. - Added Tailwind CSS as a dependency. - Integrated Tailwind CSS plugin in vite.config.ts for styling support. Used PluginOption[] assertion to tell our typescript overlord the obvious. - Created a global.css file for tailwind to target. - Refactored contents of <style/> in  index.html -> global.css file for cleanliness.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig, loadEnv, PluginOption } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ mode }) => {
   const _env = loadEnv(mode, process.cwd(), '');
   return {
     base: mode === 'production' ? '/earvibes/' : '/',
-    plugins: [react({})],
+    plugins: [react(), tailwindcss()] as PluginOption[],
   };
 });
